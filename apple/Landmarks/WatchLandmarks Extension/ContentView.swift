93724473b4764c08708e1a7ed4d5dc87dd29586b
Monrepo <wiipuri@icloud.com>
2021-01-03 15:54:23 +0300

    Some changes

--- apple/Landmarks/WatchLandmarks Extension/ContentView.swift
+++ apple/Landmarks/WatchLandmarks Extension/ContentView.swift
@@ -1,21 +1,14 @@
-//
-//  ContentView.swift
-//  WatchLandmarks Extension
-//
-//  Created by Владимир Ильин on 02.01.2021.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        LandmarkList()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
