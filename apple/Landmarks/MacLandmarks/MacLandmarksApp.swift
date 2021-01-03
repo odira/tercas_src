b2129427f75f526d497abc23f54d68af41df1fc4
Monrepo <wiipuri@icloud.com>
2021-01-04 00:01:11 +0300

    Some changes

---
+++ apple/Landmarks/MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by Владимир Ильин on 03.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
